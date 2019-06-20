72c20900b4f68647d71e7852b5dd6beed6ec8b56
um-31 <udaymahajan97@gmail.com>
2019-06-20 19:14:15 -0400

    [Udhay] Extension for units created

---
+++ C0752248_MID_TERM_MAD3004/C0752248_MID_TERM_MAD3004/Extention/Units.swift
@@ -0,0 +1,15 @@
+//
+//  Units.swift
+//  C0752248_MID_TERM_MAD3004
+//
+//  Created by hhh on 2019-06-20.
+//  Copyright © 2019 hhh. All rights reserved.
+//
+
+import Foundation
+
+extension Int{
+    public func units() -> String{
+        return String.init(format: "%d Units")
+    }
+}
