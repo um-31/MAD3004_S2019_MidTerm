dea7ad7afcb5cb2660da7d7c101ef53228e36cab
um-31 <udaymahajan97@gmail.com>
2019-06-20 18:08:55 -0400

    [Udhay] Initial Commit

---
+++ C0752248_MID_TERM_MAD3004/C0752248_MID_TERM_MAD3004/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  C0752248_MID_TERM_MAD3004
+//
+//  Created by hhh on 2019-06-20.
+//  Copyright © 2019 hhh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
